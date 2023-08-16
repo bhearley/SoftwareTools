31581d458be8dc88f0b3e71b0774f2f15b0446c4
bhearley <109361610+bhearley@users.noreply.github.com>
2023-08-16 14:53:13 -0400

    Update SoftwareTools.py

--- SoftwareTools.py
+++ SoftwareTools.py
@@ -1,4 +1,13 @@
 #Import Modules
+import pandas as pd
 import streamlit as st
+from openpyxl import load_workbook
+
+
+#Load data
+file = "C:\\Users\\bhearley\Box\\Software Tools Documentation\\Utility\\Summary.xlsx"
+DF = pd.read_excel(file)
+
 
 st.title('NASA GRC Software Tools')
+st.dataframe(DF)
